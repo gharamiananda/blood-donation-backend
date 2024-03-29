544975dbd4b6d345e736b4c016938538c19a6cfa
techwensananda <ananda.gharami@techwens.in>
2024-03-29 16:23:05 +0530

    model setup and user regiter and login done

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -24,6 +24,7 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
         }
     })
 });
+
 const registerUser = catchAsync(async (req: Request, res: Response) => {
 
     const result = await AuthServices.createUserIntoDB(req);
